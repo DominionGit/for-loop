56993bf6657e1922399b9803c744d8e5193ba9e9
DominionGit <dominionmichael2@gmail.com>
2022-05-03 01:37:47 -0800

    for-loop

---
+++ app.js
@@ -0,0 +1,11 @@
+// question 1
+
+let array1 = [20, 46, 90, 80, 120];
+let array2 = [2, 2, 5, 4, 2];
+
+function main(){
+let a = sizeof(array1)/sizeof(array1[0]);
+let b = sizeof(array2)/sizeof(array2[0]);
+calculate(array1, array2, a, b);
+return 0;
+}
